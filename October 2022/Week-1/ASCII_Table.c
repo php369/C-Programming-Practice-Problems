82d60401046bb49a883e3bcd1a08944bd1467231
Poojan Patel <patelpoojan308@gmail.com>
2022-11-01 21:50:27 +0530

    Updates

--- October 2022/Week-1/ASCII_Table.c
+++ October 2022/Week-1/ASCII_Table.c
@@ -12,7 +12,29 @@ void main()
 
     L1: if (decimal <= total)
     {
-        printf("\n%d %c", decimal, decimal);
+        printf("\n%d \t %c", decimal, decimal);
+        
+        if (decimal == 32)
+        {
+            printf("\t Whitespace");
+        }
+        else if ((decimal > 32 && decimal <=47) || (decimal >= 58 && decimal <= 64) || (decimal >= 91 && decimal <= 96) || (decimal >= 123 && decimal <= 126))
+        {
+            printf("\t Special Character");
+        }
+        else if (decimal >= 48 && decimal <= 57)
+        {
+            printf("\t Numerical");
+        }
+        else if (decimal >= 65 && decimal <= 90)
+        {
+            printf("\t Upper Case Letters");
+        }
+        else
+        {
+            printf("\t Lower Case Letters");
+        }
+
         decimal += 1;
         goto L1;
     }
