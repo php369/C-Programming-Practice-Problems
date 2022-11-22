7ba5181db6c537e1a51425accb278bd021fd5cf7
Poojan Patel <patelpoojan308@gmail.com>
2022-11-22 21:19:09 +0530

    New Practice

---
+++ October 2022/Week-3/Diamond_Star_Pattern.c
@@ -0,0 +1,56 @@
+//Question:6 (Week-3)
+/*Program to print the following triangle star pattern:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *                                */
+                                          
+#include<stdio.h>
+
+void main()
+{
+    int row, col, n, space, odd_count=1;
+
+    //Asking the user how many rows of the pattern he/she wants to print
+    printf("Enter the no. of rows upto the first half of the required diamond:\n");
+    scanf("%d", &n);
+
+    printf("\n");
+
+    //First half of the pattern
+    for (row=1; row<=n; row++)
+    {
+        //For Spaces
+        for (space=n-1; space>=row; space--)
+            printf(" ");
+
+        //For Stars
+        for (col=1; col<=odd_count; col++)
+            printf("*");
+
+        printf("\n");
+
+        odd_count += 2;
+    }
+
+    //Second half of the pattern
+    for (row=1; row<n; row++)
+    {
+        //For Spaces
+        for (space=1; space<=row; space++)
+            printf(" ");
+
+        //For Stars
+        for (col=1; col<=(odd_count-4); col++)
+            printf("*");
+
+        printf("\n");
+
+        odd_count -= 2;
+    }
+}
