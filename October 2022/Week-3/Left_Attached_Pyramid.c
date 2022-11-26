978ffaf4352028c87efae482042225202f58abfe
Poojan Patel <patelpoojan308@gmail.com>
2022-11-26 14:11:41 +0530

    New Practice

---
+++ October 2022/Week-3/Left_Attached_Pyramid.c
@@ -0,0 +1,51 @@
+//Question:15 (Week-3)
+/*Write a c program to print the following star pattern:
+  *
+  * *
+  * * *
+  * * * *
+  * * * * *
+  * * * *
+  * * *
+  * *
+  *                               */
+
+#include<stdio.h>
+
+void main()
+{
+    int n, row, col;
+
+    printf("Enter the no. of rows of the pattern you want to print: ");
+    scanf("%d", &n);
+
+    for (row=0; row<n; row++)
+    {
+        printf("\n");
+
+        for (col=0; col<n; col++)
+        {
+            if (row >= col)
+                printf("* ");
+            else
+                printf("  ");
+        }
+    }
+
+    printf("\r");
+
+    for (row=0; row<n; row++)
+    {
+        //printf("\n");
+
+        for (col=0; col<n; col++)
+        {
+            if ((row + col) <= (n - 1))
+                printf("* ");
+            else
+                printf("  ");
+        }
+
+        printf("\n");
+    }
+}
